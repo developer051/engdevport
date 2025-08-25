30d5e20f32ade0405cafd524708852616330fd19
Supachai Ocharos <engineer.socharos@gmail.com>
2025-08-25 16:14:29 +0700

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import NavBar from "./components/NavBar";
 import NavBar2 from "./components/NavBar2";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
